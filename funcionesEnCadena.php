ca0c062ee4be73361ab8a580c4341dae5dfa83fe
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-01 08:53:20 +0200

    actualizado

--- funcionesEnCadena.php
+++ funcionesEnCadena.php
@@ -1,27 +1,24 @@
 <?php
 
-$nombre = $_GET["nombre"];
-
-$prefijo = $_GET["prefijo"] ?? "";
-
-$prefijoEncontrado = stripos($nombre, $prefijo);
-
-if($prefijo !=null){
-
-    if($prefijoEncontrado === 0){
-
-        echo "El prefijo coincide con el nombre <br>";
-
-    }else{
-
-        echo "El prefijo no coincide con el nombre <br>";
-
-    }
-}
+$nombre = $_GET["nombre"] ?? "Andrea";
 
 $nombre = trim($nombre, "/");
 
 echo "Tu nombre es ".$nombre."<br>";
+
+$prefijo = $_GET["prefijo"] ?? ""; //si es null devuelve un valor, 0, 1 o false
+
+$prefijoEncontrado = stripos($nombre, $prefijo);
+
+if($prefijoEncontrado === 0){
+
+    echo "El prefijo coincide con el nombre <br>";
+
+}else{
+
+    echo "El prefijo no coincide con el nombre <br>";
+
+}
 
 echo "La longitud del nombre es: ".strlen($nombre)."<br>";
 
@@ -29,13 +26,29 @@ echo "El nombre en mayúsculas: " .strtoupper($nombre)."<br>";
 
 echo "El nombre en mayúsculas: " .strtolower($nombre)."<br>";
 
-echo "La letra 'a' aparece: " .strtoupper(substr_count($nombre, 'a')) ." veces <br >";
+echo "<br>";
 
-echo "El nombre después de la modificación: " .str_ireplace("o", "0", "nombre") ."<br>" ?? "No hay ninguna o en el nombre <br >";
+echo "La letra 'a' aparece: " .substr_count((strtoUpper($nombre)),'A') ." veces <br >";
 
-echo $prefijo;
+//posicion de la primera a
 
-echo $prefijoEncontrado;
+$posicionA=stripos($nombre, "a");
+
+if(!is_int($posicionA)){
+
+    echo "No hay ninguna 'a' en el nombre <br>";
+
+}else{
+
+    echo "La primera posición de la letra a es: " .($posicionA+1) ."<br>"; //se suma 1 porque la cadena empieza por 0
+
+}
+
+echo "El nombre después de la modificación: " .str_ireplace("o", 0, $nombre) ."<br>" ?? "No hay ninguna o en el nombre <br >";
+
+echo "<br>";
+
+echo "<h3>URL</h3>";
 
 //URL
 
