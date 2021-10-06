80b436b3c0ad130df9fd8f23a1c999e004d5ecf5
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-06 10:38:21 +0200

    actualizado6Octubre

--- sort3.php
+++ sort3.php
@@ -2,15 +2,17 @@
 
     echo "<h1>Ordenación por longitud de descripción</h1>";
 
-    $array = ["Andrea" => "Martín", "Saúl" => "Pacheco", "Carlos" => "Ramos", "Aaron" => "Ruiz"];
+    $array = ["Andrea" => "Martín", "Saúl" => "Pacheco", "Carlos" => "Ramos", "Aaron" => "Ruíz"];
+
+    echo mb_strlen('Ruíz');
 
     function comparacion ($a, $b){ //compara valores del array
 
-        if(strlen($a) == strlen($b)){
+        if(mb_strlen($a) == mb_strlen($b)){
 
             return 0;
         }
-        return (strlen($a) < strlen($b)) ? -1:1;
+        return (mb_strlen($a) < mb_strlen($b)) ? -1:1;
     }
     
     uasort($array, 'comparacion'); //callable
