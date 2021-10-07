0e5c991f003a38d1d41f5e86a6b95c739854f55e
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-07 15:13:20 +0200

    actualizado7Octubre

--- arrays/password.php
+++ arrays/password.php
@@ -2,17 +2,42 @@
 
     echo "<h1>Contraseña Random</h1>";
 
-    function randPass ($upper = 1, $lower = 5, $numeric = 3, $other){
+    $contraseña ="";
 
-        $upper = chr(rand(65, 90));
-        $lower = chr(rand(97, 122));
-        $numeric = rand(0, 999);
-        $other = chr(rand(33, 38));
+    function randPass ($upper = 1, $lower = 5, $numeric = 3, $other=2){
 
-        $contraseña = $upper.$lower.$numeric.$other;
+        $totalUpper= "";
+        $totalLower="";
+        $totalNumeric ="";
+        $totalOther="";
 
-        str_shuffle($contraseña);
+        for ($i=0; $i<$upper; $i++){
+            $totalUpper .= chr(rand(65, 90));
+        }
+
+        for ($i=0; $i<$lower; $i++){
+            $totalLower .= chr(rand(97, 122));
+        }
+
+        for ($i=0; $i<$numeric; $i++){
+            $totalNumeric .= chr(rand(48, 57));
+        }
+
+        for ($i=0; $i<$other; $i++){
+            
+            $rangoOther = [chr(rand(32, 47)), chr(rand(58, 64)), chr(rand(91, 96)), chr(rand(123, 126))];
+            
+            shuffle($rangoOther);
+
+            $totalOther .= $rangoOther[0];
+        }
+
+        return str_shuffle($totalUpper.$totalLower.$totalNumeric.$totalOther);
+
     }
 
-    randPass($upper, $lower, $numeric, $other);
+    $contraseña = randPass (1, 5, 3, 2);
+
+    echo "La contraseña es: " .$contraseña;
     
+?>
