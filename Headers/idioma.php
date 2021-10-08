d3aeaae7a4e7cfbb869abab108b9815e72645c1a
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-08 09:58:16 +0200

    actualizado8Octubre

---
+++ Headers/idioma.php
@@ -0,0 +1,63 @@
+<?php
+
+    $language = $_GET["setLanguage"] ?? "";
+
+    if ($language == ""){
+
+       $language = $_COOKIE ["language"] ?? "es";
+
+    }
+
+    setcookie("language", $language);
+
+    echo "Valor de la cookie: ".$_COOKIE["language"] ?? NULL;
+
+    $lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+
+    echo $lang;
+
+    if ($language == "en"){
+
+        $content = "This page is in English";
+
+        $title = "Change the language of the page";
+
+    }else{
+
+        $content = "Esta página está en Castellano (Idioma por defecto)";
+
+        $title = "Cambiar el idioma de la página";
+
+    }
+
+?>
+
+<!doctype html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title><?= $title ?></title>
+
+    <meta name="author" content="Víctor Ponz">
+
+</head>    
+
+<body>
+
+    <ul><?= $title ?>
+
+        <li><a href='idioma.php?setLanguage=es'>Español</a></li>
+
+        <li><a href='idioma.php?setLanguage=en'>Inglés</a></li>
+
+    </ul>
+
+    <?= $content ?>
+
+</body>
+
+</html>
