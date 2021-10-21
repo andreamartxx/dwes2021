fe598b36e54fa64beb4a47145b808531171a2a9f
Andrea Martin <78144695+andreamartxx@users.noreply.github.com>
2021-10-21 15:32:00 +0200

    Update idioma.php

--- Headers/idioma.php
+++ Headers/idioma.php
@@ -1,34 +1,26 @@
 <?php
 
-    $language = $_GET["setLanguage"] ?? "";
-
-    if ($language == ""){
-
-       $language = $_COOKIE ["language"] ?? "es";
-
-    }
-
-    setcookie("language", $language);
-
-    echo "Valor de la cookie: ".$_COOKIE["language"] ?? NULL;
-
-    $lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-
-    echo $lang;
+    if (substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2)=="es") {
+        
+        $language = "es";
+        
+    } else {
+        
+        $language = "en";
+    }    
 
     if ($language == "en"){
-
+        
         $content = "This page is in English";
-
         $title = "Change the language of the page";
-
+        
     }else{
-
+        
         $content = "Esta página está en Castellano (Idioma por defecto)";
-
         $title = "Cambiar el idioma de la página";
-
+        
     }
+
 
 ?>
 
